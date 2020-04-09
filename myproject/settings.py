6d846bb654832333527c433f19c08583bebd0ad4
Yuriy <veremeevu@ukr.net>
2020-04-09 17:47:15 +0300

    first7 commit

--- myproject/settings.py
+++ myproject/settings.py
@@ -126,5 +126,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/rest_framework/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'myapp/static/rest_framework/'),
+
+]
