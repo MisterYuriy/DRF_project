290a822c53d169c9abb7730022050f69113f908b
Yuriy <veremeevu@ukr.net>
2020-04-09 17:43:48 +0300

    first6 commit

--- myproject/settings.py
+++ myproject/settings.py
@@ -127,4 +127,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/rest_framework/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static/rest_framework/")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
