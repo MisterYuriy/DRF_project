201537fd093eedf7f900edf0bf23b375f0a8d23c
Yuriy <veremeevu@ukr.net>
2020-04-10 13:03:43 +0300

    first24 commit

--- myproject/settings.py
+++ myproject/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+#PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = 'static'
 STATIC_URL = '/static/'
 
